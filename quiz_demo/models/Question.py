e613910eb322df1270203b4e2337437d627aa41e
AMIR HASHMI <amirhashmi017@gmail.com>
2024-06-27 14:51:27 +0500

    Models Of LMS Project

--- quiz_demo/models/Question.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class Question(models.Model):
-    _name = "question"
-    _description = "Will hold question information"
-
-    quiz_id = fields.Many2one('quiz', string="Quiz", required=True, ondelete='cascade')
-    content = fields.Text("Content", required=True)
-    answer_ids = fields.One2many('answer', 'question_id', string="Answers")
--- quiz_demo/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import Quiz
-from . import Question
-from . import Answer
