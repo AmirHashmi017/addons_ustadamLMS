7c46d6a6b546131cd9d90b98a4b98c529b6f7272
AMIR HASHMI <amirhashmi017@gmail.com>
2024-07-25 19:26:31 +0500

    Created Website view for user

--- live_sessions/__manifest__.py
+++ live_sessions/__manifest__.py
@@ -18,16 +18,23 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'website'],
 
     # always loaded
     'data': [
-        'groups/group.xml',
         'security/ir.model.access.csv',
+        'views/group.xml',
         'views/live-session-view.xml',
-        'views/live-session-user-view.xml',
         'views/menu-items.xml',
+        'views/live_web.xml',
+        'views/modal_form_coach.xml',
+        'views/live_session_info_template.xml',
     ],
+'assets': {
+'web.assets_frontend': [
+'live_sessions/static/src/js/modal.js'
+]
+},
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
