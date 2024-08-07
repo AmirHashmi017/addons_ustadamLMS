cabea6199c56891e0c58be3a04ad261893b1250a
AMIR HASHMI <amirhashmi017@gmail.com>
2024-08-07 19:08:51 +0500

    Added Home Tutor Service

--- task_lyft/models/tasklyft_service.py
+++ task_lyft/models/tasklyft_service.py
@@ -6,21 +6,36 @@ class tasklyft_service(models.Model):
 
     title = fields.Char(string='Title', required=True)
     category = fields.Selection([
-        ('Software Developer', 'Software Developer'), 
-        ('Web Developer', 'Web Developer'), 
-        ('Mobile App Developer', 'Mobile App Developer'), 
-        ('Graphic Designer', 'Graphic Designer'), 
-        ('UI/UX Designer', 'UI/UX Designer'), 
-        ('Data Analyst', 'Data Analyst'), 
-        ('Odoo Developer', 'Odoo Developer'), 
-        ('Word-Press Developer', 'Word-Press Developer')
+        ('Matric', 'Matric'), 
+        ('FSC(Pre Engineering)', 'FSC(Pre Engineering)'), 
+        ('FSC(Pre Medical)', 'FSC(Pre Medical)'), 
+        ('ICS', 'ICS'), 
+        ('ICOM', 'ICOM'), 
+        ('FA', 'FA'), 
+        ('Pyhton', 'Pyhton'), 
+        ('Web Development', 'Web Development'),
+        ('C++', 'C++'),
+        ('OOP and C#', 'OOP and C#'),
+        ('ODOO Development', 'ODOO Development'),
+        ('Graphic Designing', 'Graphic Designing'),
+        ('Wordpress', 'Wordpress')
     ], string='Category', required=True)
     experience_level = fields.Selection([
         ('Beginner', 'Beginner'),
         ('Intermediate', 'Intermediate'), 
         ('Professional', 'Professional')
     ], string='Experience Level')
-    picture = fields.Image(string='Picture', required=True)    
-    price_per_hour = fields.Float(string='Price Per Hour(PKR)', required=True)
+    Location=fields.Selection([
+        ('UET Lahore','UET Lahore'),
+        ('Shalamar Garden','Shalamar Garden'),
+        ('Model Town','Model Town'),
+        ('Gulberg','Gulberg'),
+        ('Town Ship','Town Ship'),
+        ('Muslim Town','Muslim Town'),
+        ('Wahdat Road','Wahdat Road')
+        ],string='Location',required=True)
+    picture = fields.Image(string='Picture', required=True)
+
+    price_per_month = fields.Float(string='Price Per Month(PKR)', required=True)
     
     user_id = fields.Many2one('res.partner', string="User")
