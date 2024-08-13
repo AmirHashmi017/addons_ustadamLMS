d59561bb7d15074de42b63d23cffefbf05cbafed
AMIR HASHMI <amirhashmi017@gmail.com>
2024-08-13 19:49:56 +0500

    Added Contact of Service

--- task_lyft/static/src/js/search.js
+++ task_lyft/static/src/js/search.js
@@ -40,3 +40,20 @@ function toggleMap() {
         viewMapBtn.style.display = "inline-block";
     }
 }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Attach click event listener to all contact buttons
+//     document.querySelectorAll('a[data-toggle="modal"]').forEach(function(button) {
+//         button.addEventListener('click', function() {
+//             // Get data attributes from the clicked button
+//             var name = this.getAttribute('data-name');
+//             var email = this.getAttribute('data-email');
+//             var phone = this.getAttribute('data-phone');
+
+//             // Populate the modal with the fetched data
+//             document.getElementById('modalName').textContent = name;
+//             document.getElementById('modalEmail').textContent = email;
+//             document.getElementById('modalPhone').textContent = phone;
+//         });
+//     });
+// });
